b6a9e106bf6188a964ce06f03454c45f3ec1f4bf
Xavier Baró <xbaro@uoc.edu>
2014-08-19 18:11:27 +0200

    WSGI
    Modified configuration

--- ageRecognition/ageRecognition/wsgi.py
+++ ageRecognition/ageRecognition/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-import site
 import os
 import site
+
 site.addsitedir(os.path.join(os.path.dirname(os.path.abspath(__file__)),'../../venv/lib/python2.7/site-packages'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ageRecognition.settings")
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
